1aa8beac05c6bde09393e0d1cc176ca1487646b7
Manuel Grundner <m.grundner@delegate.at>
2020-09-11 15:40:06 +0200

    feat: added antdesign

--- src/Xenial.Doughnut.Client/Program.cs
+++ src/Xenial.Doughnut.Client/Program.cs
@@ -32,6 +32,8 @@ namespace Xenial.Doughnut.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            builder.Services.AddAntDesign();
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddOidcAuthentication(options =>

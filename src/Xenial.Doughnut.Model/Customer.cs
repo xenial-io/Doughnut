b58cdf732923fe5827bce00946729e8f30429e22
Manuel Grundner <m.grundner@delegate.at>
2020-09-16 01:55:54 +0200

    feat: add customers

--- src/Xenial.Doughnut.Model/Customer.cs
+++ src/Xenial.Doughnut.Model/Customer.cs
@@ -12,6 +12,41 @@ namespace Xenial.Doughnut.Model
         private string name;
         [Persistent("Name")]
         public string Name { get => name; set => SetPropertyValue(ref name, value); }
+        private string street;
+        [Persistent("Street")]
+        public string Street { get => street; set => SetPropertyValue(ref street, value); }
+
+        private string zipCode;
+        [Persistent("ZipCode")]
+        public string ZipCode { get => zipCode; set => SetPropertyValue(ref zipCode, value); }
+
+        private string city;
+        [Persistent("City")]
+        public string City { get => city; set => SetPropertyValue(ref city, value); }
+
+        // private Country _Country;
+        // [Persistent("Country")]
+        // public Country Country { get => _Country; set => SetPropertyValue(ref _Country, value); }
+
+        private string email;
+        [Persistent("Email")]
+        public string Email { get => email; set => SetPropertyValue(ref email, value); }
+
+        private string website;
+        [Persistent("Website")]
+        public string Website { get => website; set => SetPropertyValue(ref website, value); }
+
+        private string phone;
+        [Persistent("Phone")]
+        public string Phone { get => phone; set => SetPropertyValue(ref phone, value); }
+        private string comment;
+        [Persistent("Comment")]
+        [Size(SizeAttribute.Unlimited)]
+        public string Comment { get => comment; set => SetPropertyValue(ref comment, value); }
+
+        private string taxNumber;
+        [Persistent("TaxNumber")]
+        public string TaxNumber { get => taxNumber; set => SetPropertyValue(ref taxNumber, value); }
 
         [Association]
         public XPCollection<CustomerProject> Projects => GetCollection<CustomerProject>();

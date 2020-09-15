723cb21d3a7ba68274709d2942dfa4aab3c2746c
Manuel Grundner <m.grundner@delegate.at>
2020-09-15 13:22:53 +0200

    feat: activities full crud

--- src/Xenial.Doughnut.Backend/Startup.cs
+++ src/Xenial.Doughnut.Backend/Startup.cs
@@ -14,31 +14,13 @@ namespace Xenial.Doughnut.Server
 {
     public class Startup
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Startup" /> class.
-        /// </summary>
-        /// <param name="configuration">The configuration.</param>
-        /// <param name="environment">The environment.</param>
         public Startup(IConfiguration configuration, IWebHostEnvironment environment)
             => (Configuration, Environment) = (configuration, environment);
 
-        /// <summary>
-        /// Gets the configuration.
-        /// </summary>
-        /// <value>
-        /// The configuration.
-        /// </value>
         public IConfiguration Configuration { get; }
-        /// <summary>
-        /// Gets the environment.
-        /// </summary>
-        /// <value>
-        /// The environment.
-        /// </value>
+
         public IWebHostEnvironment Environment { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -60,7 +42,6 @@ namespace Xenial.Doughnut.Server
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
